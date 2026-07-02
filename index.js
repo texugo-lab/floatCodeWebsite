17ddd1e0de95a8368b80e8285c414a2d8eab4b02
Texugo <texugo13@duck.com>
2026-07-02 16:22:43 -0300

    website

--- index.js
+++ index.js
@@ -5,29 +5,46 @@ let darkmode = localStorage.getItem('darkmode');
 const themeSwitch = document.getElementById('theme-switch');
 
 const enableDarkmode = () => {
+	setAnimationsDuration("0s");
   	document.body.classList.add('darkmode');
   	localStorage.setItem('darkmode', 'active');
 }
 const disableDarkmode = () => {
+	setAnimationsDuration("0s");
   	document.body.classList.remove('darkmode');
   	localStorage.setItem('darkmode', null);
 }
 const setAnimationsDuration = (duration) => {
 	document.body.style.transitionDuration = duration;
-	document.querySelector("#theme-switch #dark-mode").style.transitionDuration = duration;
-	document.querySelector("#theme-switch #light-mode").style.transitionDuration = duration;
-	document.querySelector("header").style.transitionDuration = duration;
-	document.querySelector("nav").style.transitionDuration = duration;
+	document.querySelectorAll("#theme-switch #dark-mode").forEach((item) => {
+		item.style.transitionDuration = duration;
+	})
+	document.querySelectorAll("#theme-switch #light-mode").forEach((item) => {
+		item.style.transitionDuration = duration;
+	})
+	document.querySelectorAll("header").forEach((item) => {
+		item.style.transitionDuration = duration;
+	})
+	document.querySelectorAll("nav").forEach((item) => {
+		item.style.transitionDuration = duration;
+	})
+	document.querySelectorAll(".code").forEach((item) => {
+		item.style.transitionDuration = duration;
+	})
+	document.querySelectorAll("nav a").forEach((item) => {
+		item.style.transitionDuration = duration;
+	})
+
 }
-setAnimationsDuration("0s");
+
 if(darkmode === "active")
 	enableDarkmode();
 else
 	disableDarkmode();
 function toggleMode() {
-	setAnimationsDuration("0.7s");
 	darkmode = localStorage.getItem('darkmode');
-  	darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+	darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+	setAnimationsDuration("0.7s");
 }
 
 // End //
@@ -36,12 +53,12 @@ function openSite(site) {
 	window.open(site, "_self");
 }
 if(document.querySelector('.sidebar')){
-	document.querySelector('.sidebar').style.right = "-46vw";
+	document.querySelector('.sidebar').style.right = "-45vw";
 }
 function toggleSidebar(){
 	let sidebar = document.querySelector('.sidebar');
 	if(sidebar.style.right == "0px")
-		sidebar.style.right = "-46vw";
-	else if(sidebar.style.right == "-46vw")
+		sidebar.style.right = "-45vw";
+	else if(sidebar.style.right == "-45vw")
 		sidebar.style.right = "0px";
 }
